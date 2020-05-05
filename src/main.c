f44bff60727403dccc496af11adc98fb6ce80b88
skytomo <tomo1kawa.fullerene@gmail.com>
2020-05-05 11:52:51 +0900

    Change output file

--- src/main.c
+++ src/main.c
@@ -10,14 +10,25 @@ int main(int argc, char *argv[]) {
   int c;
   int i = 1;
   int option_index = 0;
+  char file[256];
+  char s[256];
 
   if (argc > 1) {
-    while (i < argc)
-      if (load(fopen(argv[i++], "r")) == EXIT_FAILURE) {
-        fprintf(stderr, "error: failed to read file %s\n", argv[i - 1]);
+    while (i < argc) {
+      if (load(fopen(argv[i], "r")) == EXIT_FAILURE) {
+        fprintf(stderr, "error: failed to read file %s\n", argv[i]);
       } else {
-        gencode(fopen("output.c", "w"));
+        strncpy(file, argv[i], strlen(argv[i]) - 3);
+        file[strlen(argv[i]) - 3] = '\0';
+        sprintf(s, "%s.c", file);
+        gencode(fopen(s, "w"));
+        sprintf(s, "gcc -o %s.out %s.c", file, file);
+        system(s);
+        sprintf(s, "rm %s.c", file);
+        system(s);
       }
+      i++;
+    }
   }
   return EXIT_SUCCESS;
 }
