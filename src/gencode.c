9d0fcc52ded674ef1d5b6a0138134bf9c86e75c6
skytomo <tomo1kawa.fullerene@gmail.com>
2020-05-05 11:33:51 +0900

    Fix output indent

--- src/gencode.c
+++ src/gencode.c
@@ -4,23 +4,20 @@
 #include "compiler.h"
 
 char head[] =
-    "#include <stdio.h>\n\
-#include <stdint.h>\n\
-#include <stdlib.h>\n\
-\n\
-int main(void)\n\
-{\n\
-    uint8_t mem[30000] = {0};\n\
-    uint8_t* ptr = mem;\n";
+    "#include <stdio.h>\n#include <stdint.h>\n#include <stdlib.h>\n\nint "
+    "main(void)\n{\n    uint8_t mem[524288] = {0};\n    uint8_t* ptr = mem;\n";
 char tail[] = "}\n";
 
 int gencode(FILE *fp) {
-  int i = 0;
+  int i, j;
+  int tab = 1;
   if (fp == NULL) return EXIT_FAILURE;
   fputs(head, fp);
   for (i = 0; strcmp(code[i].op, OP_END); i++) {
-    fputs("\t", fp);
-    printf("OK : %s\n", code[i].op);
+    if (!strcmp(code[i].op, OP_LOOP_END)) tab--;
+    for (j = 0; j < tab; j++) {
+      fputs("    ", fp);
+    }
     if (!strcmp(code[i].op, OP_PLUS)) {
       fprintf(fp, "(*ptr)++;");
     } else if (!strcmp(code[i].op, OP_MINUS)) {
@@ -34,6 +31,7 @@ int gencode(FILE *fp) {
     } else if (!strcmp(code[i].op, OP_INPUT)) {
       fprintf(fp, "*ptr = getchar();");
     } else if (!strcmp(code[i].op, OP_LOOP_START)) {
+      tab++;
       fprintf(fp, "while (*ptr) {");
     } else if (!strcmp(code[i].op, OP_LOOP_END)) {
       fprintf(fp, "}");
